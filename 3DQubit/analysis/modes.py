66ad399b577d31902976517764c948588a12e891
alevarsi <varsialessandro@gmail.com>
2025-11-16 11:45:10 +0100

    cavity modes analysis

---
+++ 3DQubit/analysis/modes.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("../classes")
+from data import Data
+import matplotlib.pyplot as plt
+import numpy as np
+
+input_file = "resonance_circular_cav"
+save_as = "residuals_plot"
+d = Data()
+n_modes, f_meas, f_exp = d.read_txt(input_file, spacing=";")
+residuals = f_meas - f_exp
+print(n_modes)
+print(f_meas)
+print(f_exp)
+d.fast_plot(x=n_modes, y=residuals, 
+                Title="Residuals Plot", 
+                x_title="Number of Frequencies", 
+                y_title="Residuals (GHz)",
+                point_tipe = 'o',
+                save_as=save_as)
+
