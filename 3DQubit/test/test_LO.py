a7160f7f0ec630ae1783ffec81162a5748585acf
alevarsi <varsialessandro@gmail.com>
2025-12-12 17:25:22 +0100

    update

--- 3DQubit/test/test_LO.py
+++ 3DQubit/test/test_LO.py
@@ -5,15 +5,19 @@ import time
 
 myLO1 = LO("COM11")
 myLO2 = LO("COM12")
-delta = 10
-myLO1.freq = 5.00e9 + delta
-myLO2.freq = 5.00e9
+delta = 0
+myLO1.freq = 1e8 + delta
+myLO2.freq = 1e8
 
 print(myLO1.freq)
 print(myLO2.freq)
-git 
-#myLO1.turn_off()
+ 
 time.sleep(2)
 myLO1.turn_on()
 myLO2.turn_on()
+
+#myLO1.turn_off()
+myLO2.turn_off()
+
+
 
