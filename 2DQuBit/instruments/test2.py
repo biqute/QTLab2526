8777293e4cd96536e7b3543fb2b2cc2d8effa6b6
Cecilia Krach <c.krach@campus.unimib.it>
2026-03-20 17:25:56 +0100

    test2 modified

--- 2DQuBit/instruments/test2.py
+++ 2DQuBit/instruments/test2.py
@@ -4,29 +4,49 @@ from classes import SDG, TDS, VNA
 import classes2
 import matplotlib.pyplot as plt
 import warnings
+import time
 
-#my_sdg = SDG(ip_address ='193.206.156.10')
-#par_gau = {'ampiezza': 1, 'frequenza': 10, 'media': 0, 'sigma': 1e-3}
-#sigma_ = 1e-3
-#interval = [-5*sigma_, +5*sigma_]
-#sigma_ = 0.5
-#intervals = [-7*sigma_, +7*sigma_]
-#my_sdg.upload_waveform(sigma_, name = 'singleshot', interval=interval)
-#my_sdg.set_samp(2.4e9)
+dict_par = dict(A = 1, f = 1e7, mu = 0.0, sig = 1e-7,n_sigma = 3, name = 'singleshot', plot = True, N_cycles = 1e5)
+my_lo = classes2.LO(name='COM9')
+print(my_lo.get_IDN())
+my_lo.turn_on()
+my_lo.set_freq(5000000000)
 
-#tds = TDS('3')
+#classes2.acquire_singleshot(dict_par)
+classes2.acquire_IQ(dict_par)
+'''
+tds = classes2.TDS('3')
+pars_1 = tds.acquire_all()
+print(pars_1)
+trig_1 = tds.acquire_trig()
+print(trig_1)
 
-#my_sdg.manual_trig(1e-7)
-#print(tds.get_acquire_state())
-dict_par = dict(A = 1, f = 5e7, mu = 0.0, sig = 1e-7, n_sigma = 3, name = 'singleshot', plot = True)
+
 classes2.acquire_singleshot(dict_par)
-'''sta = 0
-sto = 5000
-X = tds.acquisition(ch = 1, start = sta , stop = sto)
-print(max(X['data']), min(X['data']))
-x = np.linspace(0, (sto-sta)/X['sample_rate'], len(X['data']))
-y = np.zeros(len(x))
+
+pars_2 = tds.acquire_all()
+trig_2 = tds.acquire_trig()
+
+
+print(my_sdg.get_IDN())
+my_sdg.turn_ON(ch=1)
+my_sdg.set_arb_formwave(ch=1, wtp='sine', index=19)
+#my_sdg.upload_waveform(dict_par)
+my_sdg.set_freq(1, 3e6)
+my_sdg.set_amp(1, 1)
+time.sleep(1)
+tds = classes2.TDS('3')
+I, Q = tds.plot_acquisition(1)
+phi = []
+for i in range(len(Q['data'])):
+        phi_point = np.arctan2(Q['data'][i],I['data'][i])
+        phi.append(phi_point)
 fig, ax = plt.subplots(figsize=(10,5))
-plt.plot(x, X['data'])
-plt.plot(x, y)
+X_phi = np.linspace(0, len(phi)/I['sample_rate'], len(phi))
+z = np.ones(len(X_phi))*np.pi/2
+z_2 = np.ones(len(X_phi))*-np.pi/2
+plt.plot(X_phi, z_2, color='red', linestyle='--')
+plt.plot(X_phi, z, color='red', linestyle='--')
+plt.plot(X_phi, phi)
+plt.plot()
 plt.show()'''
