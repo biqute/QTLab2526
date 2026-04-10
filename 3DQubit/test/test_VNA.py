fa75e70bfc9ca9735f155a5853abf165d997f46a
alevarsi <varsialessandro@gmail.com>
2026-04-10 11:27:01 +0200

    misure 10 apr

--- 3DQubit/test/test_VNA.py
+++ 3DQubit/test/test_VNA.py
@@ -10,15 +10,15 @@ ip = '193.206.156.3'
 
 f_min = 7.36e9
 f_max = 7.54e9
-f_central = 7.491225e9
-f_span = 30e6
+f_central = 7.49044e9
+f_span = 40e6
 n_points = 10001
 n_means = 1
 power = 0
 ifband = 1e3
 
 
-n_misura = "600mK"
+n_misura = "700mK"
 data_file = "../T_dep/"+"2_MKID_resonance_" + n_misura
 output_file = "../T_dep/"+"2_MKID_plot_" +  n_misura
 
