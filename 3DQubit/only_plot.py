c3671ba4272b1d2f116358bbfda7adf9c0fb5f36
alevarsi <varsialessandro@gmail.com>
2026-05-15 16:15:48 +0200

    test

--- 3DQubit/only_plot.py
+++ 3DQubit/only_plot.py
@@ -10,7 +10,7 @@ plt.rcParams.update({
 # 1. Caricamento dati
 # Usiamo invalid_raise=False per ignorare automaticamente le fastidiose 
 # righe di testo in fondo al CSV di Sonnet
-data = np.loadtxt('data/IQ_mixer_data.txt', skiprows=2)
+data = np.loadtxt('data/Delta_5Hz.txt', skiprows=2)
 
 # Rimuoviamo eventuali righe vuote o header lette come NaN
 
@@ -19,9 +19,9 @@ A_data = data[:, 1]     # Parte reale di S21
 B_data = data[:, 2]     # Parte immaginaria di S21
 
 
-plt.plot(time*1e-6, A_data, label='Channel A', color='blue')
-plt.plot(time*1e-6, B_data, label='Channel B', color='darkorange')
-plt.xlabel('Time (ms)', fontsize=12)
+plt.plot(time*1e3, A_data, label='Channel A', color='blue')
+plt.plot(time*1e3, B_data, label='Channel B', color='darkorange')
+plt.xlabel('Time (us)', fontsize=12)
 plt.ylabel('Signal', fontsize=12)
 plt.title('Acquisizione.txt data', fontsize=14)
 plt.grid(True, alpha=0.3)
