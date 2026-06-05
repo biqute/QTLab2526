360161260d772bca78daf5b4033065466452bf7e
alevarsi <varsialessandro@gmail.com>
2026-06-05 11:38:30 +0200

    zero span attempt

--- 3DQubit/qubit1/2tone_spec/acquisition.py
+++ 3DQubit/qubit1/2tone_spec/acquisition.py
@@ -32,28 +32,30 @@ my_LO.set_pow(0) # Imposta una potenza iniziale (es. 0 dBm)
 # --- Configurazione VNA per la Cavità (Readout) ---
 f_min = 7.558e9
 f_max = 7.608e9
-n_points = 4001
-n_means = 5
-power = -17 # dBm
+n_points = 1001
+n_means = 3
+power = -22 # dBm
 ifband = 100
 
-my_vna.set_freq_limits(f_min, f_max)
+#my_vna.set_freq_limits(f_min, f_max)
+my_vna.set_freq_span(center=7.5833e9, span=100) # Imposta direttamente centro e span
 my_vna.set_sweep_points(n_points)
 my_vna.set_n_means(n_means)
 my_vna.set_ifband(ifband)
 my_vna.set_power(power)
 
-data_file = "b_2tone_data"
-
+data_file = "0span_data"
+f_q = 5.62476e9
+delta = 20e6
 # Range di scansione del Qubit (Pump)
-freq_list = np.arange(5.6e9, 5.65e9, 1e6) 
+freq_list = np.arange(f_q - delta/2, f_q + delta/2, 1e6) 
 
 # --- Prima di iniziare il ciclo FOR ---
 I_list = []
 Q_list = []
 
 # Acquisiamo l'asse X (frequenze cavità) una volta sola, poiché i limiti del VNA sono fissi
-freq_vna_X = my_vna.get_freq()  # Array 1D con 4001 elementi
+freq_vna_X = my_vna.get_freq()  # Array 1D con 1001 elementi
 
 my_vna.set_single_sweep_mode()
 time.sleep(1) # Un secondo di assestamento
@@ -65,9 +67,9 @@ for i, f_drive in enumerate(freq_list):
     time.sleep(0.2) 
     print("   -> Avvio sweep e calcolo medie sul VNA...")
     my_vna.start_sweep()
-    time.sleep(500)  # Attesa dello sweep delle medie
+    time.sleep(30*n_means)  # Attesa dello sweep delle medie
     
-    I, Q = my_vna.get_S_parameters() # Array da 4001 elementi ciascuno
+    I, Q = my_vna.get_S_parameters() # Array da 1001 elementi ciascuno
     
     # Salviamo la riga intera nella nostra lista
     I_list.append(I)
@@ -75,16 +77,16 @@ for i, f_drive in enumerate(freq_list):
 
 # --- CONFIGURAZIONE STRUTTURA OTTIMALE (MATRICI 2D) ---
 # Convertiamo le liste in matrici 2D reali. 
-# Dimensioni risultanti: (Numero_f_drive, 4001) -> nel tuo caso (51, 4001)
+# Dimensioni risultanti: (Numero_f_drive, 1001) -> nel tuo caso (51, 1001)
 I_matrix = np.array(I_list)
 Q_matrix = np.array(Q_list)
 
 # Salviamo gli assi indipendenti e le matrici Z in un unico file .npz
-np.savez_compressed("spettroscopia_qubit.npz", 
+np.savez_compressed(data_file+".npz", 
                     asse_x_vna = freq_vna_X,    # Asse X (Frequenza Lettura)
                     asse_y_drive = freq_list,   # Asse Y (Frequenza Qubit)
                     I_data = I_matrix,          # Componente Reale
                     Q_data = Q_matrix)          # Componente Immaginaria
 
-print("\n--> SUCCESS: Dati salvati in 'spettroscopia_qubit.npz'")
+print("\n--> SUCCESS: Dati salvati in '" + data_file + ".npz'")
 my_LO.turn_off()
